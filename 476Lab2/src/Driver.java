66a88e368f2e82802aa879933606111094bef944
ColleenRothe <colleenrothe516@gmail.com>
2017-02-02 17:34:46 -0700

    fixed

--- 476Lab2/src/Driver.java
+++ 476Lab2/src/Driver.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author colleenrothe
@@ -7,33 +8,56 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
 
 //DICTIONARY FILE SHARED BY: YI XU
-
+//MD5 research from: http://stackoverflow.com/questions/415953/how-can-i-generate-an-md5-hash/421696#421696
 public class Driver {
-    private static ArrayList <String> pwd = new ArrayList<>();              //holds the hashes to search for
-    private static String [] hashes;                                        //holds the dictionary 
+
+    private static ArrayList<String> pwd = new ArrayList<>();              //holds the hashes to search for
+    private static ArrayList<String> hashes = new ArrayList<>();           //holds the hashes to search for
     private static long startTime;                                          //start time
 
-    public static void main(String[] args) throws IOException {
-        startTime = System.currentTimeMillis();                             //start the timer
-        //add the passwords to the list, upper case because our dictionary file is in all uppercase
-        pwd.add("6f047ccaa1ed3e8e05cde1c7ebc7d958".toUpperCase());
-        pwd.add("275a5602cd91a468a0e10c226a03a39c".toUpperCase());
-        pwd.add("b4ba93170358df216e8648734ac2d539".toUpperCase());
-        pwd.add("dc1c6ca00763a1821c5af993e0b6f60a".toUpperCase());
-        pwd.add("8cd9f1b962128bd3d3ede2f5f101f4fc".toUpperCase());
-        pwd.add("554532464e066aba23aee72b95f18ba2".toUpperCase());
-  
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+        //start the timer
+        startTime = System.currentTimeMillis();
+        //add the passwords to the list
+        pwd.add("6f047ccaa1ed3e8e05cde1c7ebc7d958");
+        pwd.add("275a5602cd91a468a0e10c226a03a39c");
+        pwd.add("b4ba93170358df216e8648734ac2d539");
+        pwd.add("dc1c6ca00763a1821c5af993e0b6f60a");
+        pwd.add("8cd9f1b962128bd3d3ede2f5f101f4fc");
+        pwd.add("554532464e066aba23aee72b95f18ba2");
+
         BufferedReader reader = null;
         try {
-            reader = new BufferedReader(new FileReader("hashDictionary.txt"));//read the file
+            //read the file
+            reader = new BufferedReader(new FileReader("passwords.txt"));
             String line;
             while ((line = reader.readLine()) != null) {
-                hashes = line.split(" ");                                     //file is split by spaces
-                
+                //convert to the hash
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.reset();
+                md.update(line.getBytes());
+                byte[] digest = md.digest();
+                BigInteger big = new BigInteger(1, digest);
+                String hash = big.toString(16);
+
+                //if the array of hashes we are looking for contains this hashed line...
+                if (pwd.contains(hash)) {
+                    //Found! end the timer
+                    long endTime = System.currentTimeMillis();
+                    //print out message
+                    System.out.println("The password for hash value " + hash + " is " + line
+                            + ", it takes the program " + (((double) endTime - startTime) / 1000) + " sec to recover this password");
+
+                }
+
             }
-             
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -45,20 +69,6 @@ public class Driver {
                 ex.printStackTrace();
             }
         }
-            test();                                                 
 
-    }
-    
-    //helper method to search for hashes in the file
-    public static void test(){
-        for(int i = 0; i<hashes.length; i++){
-            if(pwd.contains(hashes[i])){                                    //if hash is found in the file,
-                long endTime = System.currentTimeMillis();                  //stop timer...found
-                System.out.println("The password for hash value "+hashes[i].toLowerCase()+" is "+hashes[i-1]+
-                        ", it takes the program "+(((double)endTime-startTime)/1000)+" sec to recover this password");
-            }
-        }
     }
 }
-
-    
